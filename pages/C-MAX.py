f97491889680783dd08989f0e99301f3bab4df85
Ansam Ali <abahkali0005@stu.kau.edu.sa>
2024-02-29 00:08:45 +0000

    updates

--- pages/C-MAX.py
+++ pages/C-MAX.py
@@ -11,7 +11,7 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
-from langchain_community.document_loaders import PyPDFLoader
+# from langchain_community.document_loaders import PyPDFLoader
 
 openai_api_key = st.secrets["openai_api_key"]
 
